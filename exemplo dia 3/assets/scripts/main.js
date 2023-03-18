921a341f36d8d99561570458308ff681be63ce8f
christmattos <ra124994@uem.br>
2023-03-18 10:07:58 -0300

    teste

---
+++ exemplo dia 3/assets/scripts/main.js
@@ -0,0 +1,30 @@
+console.log("hello world");
+a = 120;
+a = 30;
+a = a + a;
+console.log(a);
+
+b = 123;
+
+if (b == 123){
+    c = 1234;
+}
+else{
+    c = 4321;
+}
+
+console.log(c);
+
+d = 4;
+
+switch(d){
+    case 1:
+        console.log("d vale 1");
+    break;
+    case 2:
+        console.log("d vale 2");
+    break;
+    default:
+        console.log("d é diferente de 1 ou 2")
+    break;
+}
