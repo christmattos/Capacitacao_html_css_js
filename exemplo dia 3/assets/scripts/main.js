cd2434f9c13516db9d963352a721b05f0cac66d1
christmattos <ra124994@uem.br>
2023-03-18 11:45:24 -0300

    calculadorazinha

--- exemplo dia 3/assets/scripts/main.js
+++ exemplo dia 3/assets/scripts/main.js
@@ -28,3 +28,26 @@ switch(d){
         console.log("d é diferente de 1 ou 2")
     break;
 }
+
+document.getElementById("botao").onclick = function () {
+    let primeiro = document.getElementById("primeironumero");
+    let segundo = document.getElementById("segundonumero");
+    const sb = document.getElementById("select");
+    const index = sb.selectedIndex;
+    var output;
+    if (index == 0){
+        output = (parseFloat(primeiro.value) + parseFloat(segundo.value));
+    }
+    else if (index == 1){
+        output = (parseFloat(primeiro.value) - parseFloat(segundo.value));
+    }
+    else if (index == 2){
+        output = (parseFloat(primeiro.value) * parseFloat(segundo.value));
+    }
+    else if (index == 3){
+        output = (parseFloat(primeiro.value) / parseFloat(segundo.value));
+    }
+    console.log(output);
+    document.getElementById('output').innerHTML = +output;
+};
+
